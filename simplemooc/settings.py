f4f214a13d3d2380930ced40dae44dbdf37b29bd
Dbaelvis <dbaelvis@gmail.com>
2020-03-01 11:40:35 -0300

    MediaCling

--- simplemooc/settings.py
+++ simplemooc/settings.py
@@ -171,8 +171,10 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
 
-STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 try:
     from simplemooc.local_settings import *
--- simplemooc/wsgi.py
+++ simplemooc/wsgi.py
@@ -9,10 +9,11 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 
 import os
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "simplemooc.settings")
 
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+
+application = Cling(MediaCling(get_wsgi_application()))
